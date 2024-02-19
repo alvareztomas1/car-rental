7d39329e36f0f0c36fcc5f2e6e48c0991f9e38d9
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-02-19 15:34:14 -0300

    Adds: Create an AbstractController and CarController for car's module
    AbstractController is designed to serve as a base template for creating specific controllers such as CarController. Also, it wont be instantiated due the AbstractControllerError

---
+++ src/module/car/controller/carController.js
@@ -0,0 +1,9 @@
+const AbstractController = require("./abstractController");
+module.exports = class CarController extends AbstractController {
+	constructor(uploadMiddleware, carService) {
+		super();
+
+        
+
+	}
+};
