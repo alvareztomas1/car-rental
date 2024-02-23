fdbc4d33b15b47fdd864ea6c99bbaa5fc177edd1
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-02-23 16:27:44 -0300

    Change: Reorder  index.html and view .html directories
    For a better organization, reorders this two file paths.
    
    - In index.html:
    Adds a new custom class for car cards, just for changing the background-color. Also, the car details buttons now has href "/view/car/:id", meaning that the car detail route will be "/view/car/:id" in carController
    
    - In view.html:
    Starts to separate car propieties into nunjucks inclusions

--- src/module/car/controller/carController.js
+++ src/module/car/controller/carController.js
@@ -12,13 +12,14 @@ module.exports = class CarController extends AbstractController {
 	configureRoutes(app) {
 		const ROUTE = this.ROUTE_BASE;
 		app.get(`${ROUTE}`, this.index.bind(this));
-		app.get(`${ROUTE}/:id`, this.view.bind(this));
+		app.get(`${ROUTE}/view/:id`, this.view.bind(this));
+		
 	}
 	async index(req, res){
 		const cars = await this.carService.getAll();
 		const { errors, messages } = req.session;
 
-		res.render("car/view/index.html", { data: { cars }, errors, messages });
+		res.render("car/view/index/index.html", { data: { cars }, errors, messages });
 
 		req.session.errors = [];
 		req.session.message = [];
@@ -33,12 +34,12 @@ module.exports = class CarController extends AbstractController {
 
 		try{
 			const car = await this.carService.getById(id);
-			res.render("car/view/view.html", { data: { car } });
+			res.render("car/view/view/view.html", { data: { car } });
 		}catch(e){
 			req.session.errors = [e.message, e.stack];
 			res.redirect("/car");
 		}
 
 	}
-
+	
 };
