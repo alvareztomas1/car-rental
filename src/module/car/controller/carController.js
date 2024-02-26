500517996bd85ff2e6fcedc5635d0b0a4661d571
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-02-26 20:43:30 -0300

    Adds: Sets up the carController in "add a team" route.
    Strating with:
    
    - carController:
    - Configures the route, adding the create method, thats only render the form.
    - Also save method, that will be executed when the user posts the form, mapping the car data entered into an entity.
    - Finally, checks if there is a car logo, obtains the saved car with carService and notificates the user with the successfull operation.
    -  If saving the car returns an error, user will be redirected to form again

--- src/module/car/controller/carController.js
+++ src/module/car/controller/carController.js
@@ -1,5 +1,6 @@
 const AbstractController = require("./abstractController");
 const CarIdNotDefinedError = require("./error/carIdNotDefinedError");
+const { fromDataToEntity } = require("../mapper/carMapper");
 
 module.exports = class CarController extends AbstractController {
 	constructor(uploadMiddleware, carService) {
@@ -13,9 +14,12 @@ module.exports = class CarController extends AbstractController {
 		const ROUTE = this.ROUTE_BASE;
 		app.get(`${ROUTE}`, this.index.bind(this));
 		app.get(`${ROUTE}/view/:id`, this.view.bind(this));
-		
+		app.get(`${ROUTE}/create`, this.create.bind(this));
+		app.post(`${ROUTE}/create`, this.uploadMiddleware.single("image"), this.save.bind(this));
+
+
 	}
-	async index(req, res){
+	async index(req, res) {
 		const cars = await this.carService.getAll();
 		const { errors, messages } = req.session;
 
@@ -25,21 +29,45 @@ module.exports = class CarController extends AbstractController {
 		req.session.message = [];
 
 	}
-	async view(req, res){
+	async view(req, res) {
 		const id = req.params.id;
 
-		if(!id){
+		if (!id) {
 			throw new CarIdNotDefinedError("Car id not defined");
 		}
 
-		try{
+		try {
 			const car = await this.carService.getById(id);
 			res.render("car/view/view/view.html", { data: { car } });
-		}catch(e){
+		} catch (e) {
 			req.session.errors = [e.message, e.stack];
 			res.redirect("/car");
 		}
 
 	}
-	
+	async create(req, res) {
+		res.render("car/view/form/form.html");
+	}
+	async save(req, res) {
+		try {
+			const car = fromDataToEntity(req.body);
+
+			if (req.file) {
+				const { path } = req.file;
+				car.image = path.replace("public", "");
+			}
+			const savedCar = await this.carService.save(car);
+
+			if (car.id) {
+				req.session.messages = [`${car.id} edited succesfully`];
+			} else {
+				req.session.messages = [`${savedCar.brand} ${savedCar.model} ${savedCar.year} added succesfully`];
+			}
+
+			res.redirect("/car");
+		} catch (e) {
+			res.render("car/view/form/form.html");
+
+		}
+	}
 };
