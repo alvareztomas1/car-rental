0596555a49a51a21486aa37eeb932e265555ec56
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-03-05 16:46:36 -0300

    Adds getById method to reserveRepository
    Adds this new method which obtains the required reservation by the received id, checks if the reserve obtained isn't undefined, that case meaning that the reserve was obtained successfuly, returning a Reserve entity with the fromDbToEntity mapper. Else, it throws an error.

--- src/module/reserve/repository/reserveRepository.js
+++ src/module/reserve/repository/reserveRepository.js
@@ -1,9 +1,31 @@
 const AbstractRepository = require("../../car/repository/abstractRepository");
+const ReserveNotFoundError = require("./error/reserveNotFoundError");
 
 module.exports = class ReserveRepository extends AbstractRepository {
-	constructor(mainDataBaseAdapter){
+	constructor(mainDataBaseAdapter) {
 		super();
 		this.mainDataBaseAdapter = mainDataBaseAdapter;
 	}
+
+
+	getById(id) {
+		const reserve = this.mainDataBaseAdapter.prepare(
+			`SELECT 
+			id,
+			fk_car_id,
+			since,
+			until
+			FROM reserves 
+			WHERE id = ?;`
+		).get(id);
+
+		
+		if(reserve === undefined) {
+			throw new ReserveNotFoundError("Reserve not found");
+		}
+
+		return fromDbToEntity(reserve);
+	}
+	
 };
 
