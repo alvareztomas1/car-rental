44d25fe5f1f248a34533f860ed3cb999d830533a
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-03-05 18:47:51 -0300

    Adds "save a reserve" method in Reserve Repository
    First, verifies if the operatin is an update or not, if its not:
    
    - Begins a transaction, because two statements must be executed, creating the new reserve and changing the car.reserved value from "0" to "1".
    - Executes INSERT or ROLLBACK  from new reserve.
    - Executes INSERT or ROLLBACK to change the car.reserved value from "0" to "1".
    - Executes the commit if neither ROLLBACK was executed.
    
    Both operation must be executed at the same time because:
    
    - If the "create a new reserve" fails and "set car.reserved to 1" does not, a car will be displayed as "reserved" when there isn't any reserve.
    - If the "set car.reserved to 1" fails and "create a new reserve" does not, a reserve will be displayed when the car isn't shown as "reserved".

--- src/module/reserve/repository/reserveRepository.js
+++ src/module/reserve/repository/reserveRepository.js
@@ -1,4 +1,5 @@
 const AbstractRepository = require("../../car/repository/abstractRepository");
+const { fromDbToEntity } = require("../mapper/reserveMapper");
 const ReserveNotFoundError = require("./error/reserveNotFoundError");
 
 module.exports = class ReserveRepository extends AbstractRepository {
@@ -19,13 +20,38 @@ module.exports = class ReserveRepository extends AbstractRepository {
 			WHERE id = ?;`
 		).get(id);
 
-		
-		if(reserve === undefined) {
+
+		if (reserve === undefined) {
 			throw new ReserveNotFoundError("Reserve not found");
 		}
 
 		return fromDbToEntity(reserve);
 	}
-	
+	save(reserve) {
+		let id;
+		const isUpdate = reserve.id;
+
+		if (isUpdate) {
+			// TODO: EDIT RESERVE
+		} else {
+			const transaction = this.mainDataBaseAdapter.prepare("BEGIN TRANSACTION;");
+			transaction.run();
+
+			const reserveStatement = this.mainDataBaseAdapter.prepare("INSERT OR ROLLBACK INTO reserves (fk_car_id, since, until) VALUES (?, ?, ?);");
+			const reserveStatementValues = [reserve.carId, reserve.since, reserve.until];
+			reserveStatement.run(reserveStatementValues);
+
+			const reserveCar = this.mainDataBaseAdapter.prepare("UPDATE OR ROLLBACK cars SET reserved = 1 WHERE id = ?;");
+			const reserveCarValues = [reserve.carId];
+			reserveCar.run(reserveCarValues);
+
+			const commitTransaction = this.mainDataBaseAdapter.prepare("COMMIT;");
+			const reserveResult = commitTransaction.run();
+
+			id = reserveResult.lastInsertRowid;
+		}
+
+		return this.getById(id);
+	}
 };
 
