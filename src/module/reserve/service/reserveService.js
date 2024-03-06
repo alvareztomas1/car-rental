eb0df73722e380dcb0b9dda9417674ae4540244a
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-03-06 15:06:26 -0300

    Adds two methods in ReserveService: getAll and calculateCost
    - getAll:
    Just returns the received data by calling reserveRepository.getAll method
    
    - calculateCost:
    Receives the dates of reserve and the price per day of the car. Also, calculates how much days the car will be reserved and returns result of multiplying the number of reserved days by the car's price per day

--- src/module/reserve/service/reserveService.js
+++ src/module/reserve/service/reserveService.js
@@ -6,12 +6,20 @@ module.exports = class ReserveService extends AbstractService {
 		super();
 		this.reserveRepository = reserveRepository;
 	}
+	getAll(){
+		return this.reserveRepository.getAll();
+	}
 	save(reserve) {
 		if(reserve === undefined){
 			throw new ReserveNotDefinedError("Reserve is not defined");
 		}
 
 		return this.reserveRepository.save(reserve);
+	}
+
+	calculateCost(since, until, carPrice){
+		const days = Math.floor((new Date(until) - new Date(since)) / (1000 * 60 * 60 * 24));
+		return Math.floor(days * carPrice);
 	}
 };
 
