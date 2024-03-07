058a52a43003d3cabf6d80543b7b413e1bb35347
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-03-07 16:05:21 -0300

    Adds SQLite scripts to create cars and reserves tables
    This commit introduces SQLite scripts responsible for creating tables for cars and reserves in the database.
    
    The "setup" script includes the creation of the following tables:
    
    cars: This table stores information about cars available for reservation. Each car entry includes details such as brand, model, year, transmission type, seating capacity, number of doors, presence of air conditioning, trunk size, fuel type, price, image URL, description, reservation status, creation timestamp, and last update timestamp.
    
    reserves: This table stores information about reservations made for cars. Each reserve entry includes the ID of the reserved car, start and end dates of the reservation, creation timestamp, and last update timestamp. Additionally, a foreign key ensures reference between the reserves table and the cars table.
    
    The "insert" script also includes "insert" statements to populate the cars table with initial values.

---
+++ src/config/setup.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS cars;
+DROP TABLE IF EXISTS reserves;
+
+CREATE TABLE IF NOT EXISTS cars (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    brand TEXT NOT NULL,
+    model TEXT NOT NULL,
+    car_year INTEGER NOT NULL,
+    transmission TEXT NOT NULL,
+    seats INTEGER NOT NULL CHECK (seats BETWEEN 2 AND 5),
+    doors INTEGER NOT NULL CHECK (doors BETWEEN 2 AND 5),
+    air_conditioning INTEGER CHECK (air_conditioning IN (0, 1)),
+    trunk INTEGER NOT NULL,
+    fuel TEXT NOT NULL,
+    price INTEGER NOT NULL,
+    unlimited_mileage INTEGER CHECK (unlimited_mileage IN (0, 1)),
+    car_image TEXT NOT NULL,
+    car_description TEXT NOT NULL,
+    reserved INTEGER CHECK (reserved IN (0, 1)),
+    created_at DATE DEFAULT (datetime('now', 'localtime')) NOT NULL,
+    updated_at DATE DEFAULT (datetime('now', 'localtime')) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS reserves (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    fk_car_id INTEGER NOT NULL,
+    since DATE NOT NULL,
+    until DATE NOT NULL,
+    created_at DATE DEFAULT (datetime('now', 'localtime')) NOT NULL,
+    updated_at DATE DEFAULT (datetime('now', 'localtime')) NOT NULL,
+    FOREIGN KEY (fk_car_id) REFERENCES cars (id)
+);
