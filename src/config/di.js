2c52f2ff1ff1520978623f7421a0d9523f9abc13
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-03-06 21:06:42 -0300

    Adds validations method in ReserveService
    Validation method requires carService as a dependency. This is necessary because the validation for car ID checks if the ID corresponds to a car in the database.
    
    validateDate method verifies if the date is in the format "yyyy-mm-dd" and checks if the received date is within the current year or the next year. Additionally, it validates the days and months.

--- src/config/di.js
+++ src/config/di.js
@@ -55,7 +55,7 @@ function addCarModuleDefinitions(container){
 function addReserveModuleDefinitions(container){
 	container.addDefinitions({
 		ReserveController: object(ReserveController).construct(get("CarService"), get("ReserveService")),
-		ReserveService: object(ReserveService).construct(get("ReserveRepository")),
+		ReserveService: object(ReserveService).construct(get("ReserveRepository"), get("CarService")),
 		ReserveRepository: object(ReserveRepository).construct(get("MainDatabaseAdapter")),
 	});
 }
--- src/module/reserve/service/reserveService.js
+++ src/module/reserve/service/reserveService.js
@@ -2,9 +2,10 @@ const AbstractService = require("../../car/service/abstractService");
 const ReserveNotDefinedError = require("./error/reserveNotDefinedError");
 
 module.exports = class ReserveService extends AbstractService {
-	constructor(reserveRepository) {
+	constructor(reserveRepository, carService) {
 		super();
 		this.reserveRepository = reserveRepository;
+		this.carService = carService;
 	}
 	getAll(){
 		return this.reserveRepository.getAll();
@@ -29,5 +30,47 @@ module.exports = class ReserveService extends AbstractService {
 
 		return this.reserveRepository.delete(id);
 	}
-};
 
+	validate(data){
+
+		const validation = {};
+
+		Object.keys(data).forEach((field) => {
+			validation[`${field}`] = this.validateField(field, data[field]);
+		});
+
+		return validation;
+	}
+
+	validateField(type, input){
+		
+		switch(type){
+		case "since":
+			return this.validateDate(input);
+		case "until":
+			return this.validateDate(input);
+		case "car-id":
+			return this.carService.getById(input) ? true : false;
+		}
+	}
+
+	validateDate(date){
+		const match = date.match(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/);
+		if (!match) return false;
+
+		const year = parseInt(match[0]);
+		const month = parseInt(match[1]);
+		const day = parseInt(match[2]);
+
+		const currentYear = new Date().getFullYear();
+
+		if (year < currentYear || year > currentYear + 1) return false;
+
+		const daysInMonth = new Date(year, month, 0).getDate();
+		if (isNaN(year) || isNaN(month) || isNaN(day) || day < 1 || day > daysInMonth) {
+			return false;
+		}
+
+		return true;
+	}
+};
