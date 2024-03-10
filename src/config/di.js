cf1dc6505824e269caf502c1b5c5f6b54818292d
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-03-10 13:46:59 -0300

    Adds a CarModel to Car module
    - in carModel.js: Sets up the CarModel class with a static method called "setup", that will received the sequelize instance from DIcontainer, representes the Car entity in a Cars table on the databse with Sequelize. Defines every Car entity properties and datatype with CarModel.init
    
    - in module.js: Adds CarModle to Car module's bootstrap, being able to export everything from this file.
    
    -in di.js: Requires Sequelize class, adding a funcion "configureMainSequelizeAdapter" that configures it. Also the "configureCarModel" function, that calls CarModel.setup with Sequelize instance. Finally, adds Sequelize as a common definition and CarModel to CarModule definitions. CarRepository now has CarModel as a dependency instead of better-sqlite3 main adapter

--- src/config/di.js
+++ src/config/di.js
@@ -1,15 +1,25 @@
-const { CarController, CarService, CarRepository } = require("../module/car/module");
+const { CarController, CarService, CarRepository, CarModel } = require("../module/car/module");
 const { ReserveController, ReserveService, ReserveRepository } = require("../module/reserve/module");
 const { default: DIContainer, object, get, factory } = require("rsdi");
 const session = require("express-session");
 const multer = require("multer");
 const path = require("path");
 const Sqlite3Database = require("better-sqlite3");
+const { Sequelize } = require("sequelize");
 
 function configureMainDatabaseAdapter(){
 	return new Sqlite3Database(process.env.DB_path,{
 		verbose: console.log
 	});
+}
+
+function configureMainSequelizeAdapter(){
+	const sequelize = new Sequelize({
+		dialect: "sqlite",
+		storage: process.env.DB_path
+	});
+
+	return sequelize;
 }
 
 function configureSession(){
@@ -37,18 +47,25 @@ function configureMulter(){
 	return multer({ storage });
 }
 
+function configureCarModel(container){
+	CarModel.setup(container.get("Sequelize"));
+	return CarModel;
+}
+
 function addCommonDefinitions(container){
 	container.addDefinitions({
 		MainDatabaseAdapter: factory(configureMainDatabaseAdapter),
 		Multer: factory(configureMulter),
 		Session: factory(configureSession),
+		Sequelize: factory(configureMainSequelizeAdapter),
 	});
 }
 function addCarModuleDefinitions(container){
 	container.addDefinitions({
 		CarController: object(CarController).construct(get("Multer"), get("CarService")),
 		CarService: object(CarService).construct(get("CarRepository")),
-		CarRepository: object(CarRepository).construct(get("MainDatabaseAdapter")),
+		CarRepository: object(CarRepository).construct(get("CarModel")),
+		CarModel: factory(configureCarModel),
 	});
 }
 
---
+++ src/module/car/model/carModel.js
@@ -0,0 +1,84 @@
+const { Sequelize, Model: AbstractModel, DataTypes } = require("sequelize");
+
+module.exports = class CarModel extends AbstractModel{
+	static setup(sequelizeInstance){
+		CarModel.init(
+			{
+				id: {
+					type: DataTypes.INTEGER,
+					primaryKey: true,
+					autoIncrement: true,
+					allowNull: false,
+					unique: true
+				},
+				brand: {
+					type: DataTypes.STRING,
+					allowNull: false
+				},
+				model: {
+					type: DataTypes.STRING,
+					allowNull: false
+				},
+				car_year: {
+					type: DataTypes.INTEGER,
+					allowNull: false
+				},
+				transmission: {
+					type: DataTypes.STRING,
+					allowNull: false
+				},
+				seats: {
+					type: DataTypes.STRING,
+					allowNull: false
+				},
+				doors: {
+					type: DataTypes.STRING,
+					allowNull: false
+				},
+				air_conditioning: {
+					type: DataTypes.BOOLEAN,
+					allowNull: false
+				},
+				trunk: {
+					type: DataTypes.INTEGER,
+					allowNull: false
+				},
+				fuel: {
+					type: DataTypes.STRING,
+					allowNull: false
+				},
+				price: {
+					type: DataTypes.INTEGER,
+					allowNull: false
+				},
+				unlimited_mileage: {
+					type: DataTypes.BOOLEAN,
+					allowNull: false
+				},
+				car_image: {
+					type: DataTypes.STRING,
+					allowNull: false
+				},
+				car_description: {
+					type: DataTypes.STRING,
+					allowNull: false
+				},
+				created_at: {
+					type: DataTypes.DATE,
+					defaultValue: Sequelize.NOW
+				},
+				updated_at: {
+					type: DataTypes.DATE,
+					defaultValue: Sequelize.NOW
+				}
+			},
+			{
+				sequelize: sequelizeInstance,
+				modelName: "Car",
+				timestamps: false
+			}
+		);
+
+	}
+
+};
