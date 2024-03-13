d152512369c1af6e2b58616e2efa942bc8a111c4
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-03-13 11:14:28 -0300

    Adds index, create and save methods in UserController
    - Index: Attempts to get all users in the database: If successful, renders a list of users. Otherwise, notifies an error message using req.session and redirects to the "/" route.
    
    - Create: Renders the form for adding a new user.
    
    - Save: Maps the data submitted via the form and transforms it into a User entity. Then, attempts to save the user into the database. If successful, notifies a success message using req.session and redirects to "/". If not, notifies an error message using req.session and redirects to "/".

--- src/module/user/controller/userController.js
+++ src/module/user/controller/userController.js
@@ -1,7 +1,47 @@
 const AbstractController = require("./error/abstractControllerError");
+const { fromDataToUserEntity } = require("../mapper/userMapper");
+
 module.exports = class UserController extends AbstractController{
 	constructor(userService){
 		super();
 		this.ROUTE_BASE = "/user";
+		this.userService = userService;
+	}
+
+	configureRoutes(app){
+		const ROUTE = this.ROUTE_BASE;
+		app.get(`${ROUTE}/list`, this.index.bind(this));  
+		app.get(`${ROUTE}/create`, this.create.bind(this));
+		app.post(`${ROUTE}/create`, this.save.bind(this));      
+	}
+
+	async index(req, res){
+		try{
+			const users = await this.userService.getAll();
+			res.render("user/view/index.html", { data: { users }, pageTitle: "Users list" });
+		}catch(e){
+			req.session.errors = [e.message, e.stack];
+			res.redirect("/");
+		}
+	}
+
+	create(req, res){
+		res.render("user/view/form/form.html", { pageTitle: "Add new user" });
+	}
+
+	async save(req, res){
+		const formData = fromDataToUserEntity(req.body);
+		try{
+			const savedUser = await this.userService.save(formData);
+
+			// TODO: Add validation
+
+			req.session.messages = [`Use #${savedUser.id} added succesfully`];
+			res.redirect("/");
+			
+		}catch(e){
+			req.session.errors = [e.message, e.stack];
+			res.redirect("/");
+		}
 	}
 };
