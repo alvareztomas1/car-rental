b5fd3c82e61da9b538a8c9ca26fbeda5b74294d4
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-03-13 11:17:48 -0300

    Adds getAll and save methods in UserService
    - getAll: Returns the User entities list from User Repository
    
    - save: Frist, verifies if the user is defined, if not, throws an error, else returns the saved user entitty with User Repository save method.

--- src/module/user/service/userService.js
+++ src/module/user/service/userService.js
@@ -1,6 +1,22 @@
 const AbstractService = require("./abstractService");
+const UserNotDefinedError = require("./error/userNotDefinedError");
+
 module.exports = class UserService extends AbstractService {
 	constructor(userRespotiroy) {
 		super();
+		this.userRespotiroy = userRespotiroy;
+	}
+
+	getAll(){
+		return this.userRespotiroy.getAll();
+	}
+
+	save(user) {
+		
+		if (user === undefined) {
+			throw new UserNotDefinedError("User is not defined");
+		}
+
+		return this.userRespotiroy.save(user);
 	}
 };
