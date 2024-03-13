aec952e6144aae287c146572e0ca6ee0f09ffd4a
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-03-13 11:38:29 -0300

    Adds getAll, getById and save methods in UserRepository
    - getAll: Find all users in user table, excluding the "created_at" and "updated_at" columns since they are not used in the web. If the array received has a length of "0", it indicates that no users are uploaded. Otherwise, maps every user and transforms it into a User Entity.
    
    - getById: Find an user in the user table with the id received, excluding "created_at" and "updated_at". If user isn't defeined throws an error. Otherwise, it returns a User entity with the data received.
    
    - save: Sets the received data into model format, verifies if the record is new or not, then builds the user and saves it. Finally, uses the id of the saved user to return a User entity with the getById method.

---
+++ src/module/user/repository/error/userNotFoundError.js
@@ -0,0 +1,5 @@
+module.exports = class UserNotFoundError extends Error {
+	constructor(message) {
+		super(message);
+	}
+};
--- src/module/user/repository/userRepository.js
+++ src/module/user/repository/userRepository.js
@@ -1,6 +1,63 @@
 const AbstractRepository = require("./abstractRepository");
+const { fromModelToUserEntity } = require("../mapper/userMapper");
+const UserNotFoundError = require("./error/userNotFoundError");
+
 module.exports = class UserRepository extends AbstractRepository {
 	constructor(userModel) {
 		super();
+		this.userModel = userModel;
+	}
+
+	async getAll() {
+		const users = await this.userModel.findAll({
+			attributes: {
+				exclude: ["created_at", "updated_at"]
+			}
+		});
+
+		if (users.length === 0) {
+			return false;
+		}
+
+		return users.map((user) => fromModelToUserEntity(user.toJSON()));
+	}
+
+	async getById(id) {
+		const user = await this.userModel.findByPk(id, {
+			attributes: {
+				exclude: ["created_at", "updated_at"]
+			}
+		});
+
+		if (user === undefined) {
+			throw new UserNotFoundError("User not found");
+		}
+
+		return fromModelToUserEntity(user.toJSON());
+	}
+
+	async save(user) {
+		const userToSave = {
+			id: user.id ? user.id : null,
+			names: user.names,
+			surnames: user.surnames,
+			personal_id_type: user.personalIdType,
+			personal_id_number: user.personalIdNumber,
+			nationality: user.nationality,
+			email: user.email,
+			phone: user.phone,
+			birthdate: user.birthdate,
+			address: user.address
+		};
+
+		const buildOptions = {
+			isNewRecord: !user.id
+		};
+
+		let userModel = this.userModel.build(userToSave, buildOptions);
+		userModel = await userModel.save();
+		const result = await this.getById(userModel.id);
+
+		return result;
 	}
 };
