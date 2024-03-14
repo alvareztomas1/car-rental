23187a7c8ccd04ecbd8f2b16be02b0774bdee99f
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-03-14 11:18:57 -0300

    Updates sql scripts
    Updates the seats and doors datatype according to the datatypes defined in the Car model. Additionally, removes the reserves setup, primarily due to a discrepancy in the date datatype between SQLite and Sequelize, causing an error in the reserves mapper if the reserves were created using this script.

--- src/config/setup.sql
+++ src/config/setup.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS cars;
-DROP TABLE IF EXISTS reserves;
 
 CREATE TABLE IF NOT EXISTS cars (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -7,8 +6,8 @@ CREATE TABLE IF NOT EXISTS cars (
     model TEXT NOT NULL,
     car_year INTEGER NOT NULL,
     transmission TEXT NOT NULL,
-    seats INTEGER NOT NULL CHECK (seats BETWEEN 2 AND 5),
-    doors INTEGER NOT NULL CHECK (doors BETWEEN 2 AND 5),
+    seats TEXT NOT NULL,
+    doors TEXT NOT NULL,
     air_conditioning INTEGER CHECK (air_conditioning IN (0, 1)),
     trunk INTEGER NOT NULL,
     fuel TEXT NOT NULL,
@@ -18,13 +17,4 @@ CREATE TABLE IF NOT EXISTS cars (
     car_description TEXT NOT NULL,
     created_at DATE DEFAULT (datetime('now', 'localtime')) NOT NULL,
     updated_at DATE DEFAULT (datetime('now', 'localtime')) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS reserves (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    fk_car_id INTEGER NOT NULL,
-    since DATE NOT NULL,
-    until DATE NOT NULL,
-    created_at DATE DEFAULT (datetime('now', 'localtime')) NOT NULL,
-    updated_at DATE DEFAULT (datetime('now', 'localtime')) NOT NULL,
-    FOREIGN KEY (fk_car_id) REFERENCES cars (id)
 );
